0ce41d812f3934769b6e82966dafc07461c982ac
Thiago Denny <thiagodenny@outlook.com>
2020-04-09 12:14:33 -0300

    Convert item from hooks to classes

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,35 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import '../styles/components/Item.css';
 
-const Item = ({ item, addItemWishlist }) => {
-  const { id, volumeInfo } = item;
-  const {
-    imageLinks,
-    title,
-    infoLink,
-    description,
-  } = volumeInfo;
-  const { smallThumbnail } = imageLinks || [undefined];
+class Item extends Component {
+  constructor({ item, addItemWishlist }) {
+    super();
+    this.addItemWishlist = addItemWishlist;
+    this.item = item;
+    this.id = this.item.id;
+    this.volumeInfo = this.item.volumeInfo;
 
-  const defaultUrl = 'https://sciences.ucf.edu/psychology/wp-content/uploads/sites/63/2019/09/No-Image-Available.png';
+    this.imageLinks = this.volumeInfo.imageLinks;
+    this.title = this.volumeInfo.title;
+    this.infoLink = this.volumeInfo.infoLink;
+    this.description = this.volumeInfo.description;
 
-  return (
-    <div key={id} className="item">
-      <div className="card">
-        <a className="item-title" href={infoLink}>{title}</a>
-        <div className="container">
-          <div className="container-image">
-            <input className="item-image" type="image" src={smallThumbnail || defaultUrl} alt="Logo" onClick={() => addItemWishlist(id)} />
-          </div>
-          <div className="container-description">
-            <span className="item-description">{description}</span>
+    this.smallThumbnail = (this.imageLinks ? this.imageLinks.smallThumbnail : undefined);
+    this.defaultUrl = 'https://sciences.ucf.edu/psychology/wp-content/uploads/sites/63/2019/09/No-Image-Available.png';
+  }
+
+  render() {
+    return (
+      <div key={this.id} className="item">
+        <div className="card">
+          <a className="item-title" href={this.infoLink}>{this.title}</a>
+          <div className="container">
+            <div className="container-image">
+              <input className="item-image" type="image" src={this.smallThumbnail || this.defaultUrl} alt="Logo" onClick={() => this.addItemWishlist(this.id)} />
+            </div>
+            <div className="container-description">
+              <span className="item-description">{this.description}</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 Item.propTypes = {
   item: PropTypes.shape({
