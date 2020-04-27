b6aa3ebb1c8d82d5668124ad734c23d138c9afbe
Thiago Denny <thiagodenny@outlook.com>
2020-04-27 10:39:52 -0300

    Change wishlist name

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Header = () => (
   <div className="header">
     <Link className="header__link" to="/">Home</Link>
-    <Link className="header__link" to="/wishlists">Wishlists</Link>
+    <Link className="header__link" to="/wishlists">Lists</Link>
   </div>
 );
 
