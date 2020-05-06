114c92f0c6212a6130e1c483bfb07b40d856447a
Thiago Denny <thiagodenny@outlook.com>
2020-05-06 15:01:17 -0300

    Add header style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Header.scss';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => (
   <div className="header">
-    <Link className="header__link" to="/">Home</Link>
-    <Link className="header__link" to="/wishlists">Lists</Link>
+    <NavLink className="header__link" exact to="/">Home</NavLink>
+    <NavLink className="header__link" to="/wishlists">Lists</NavLink>
   </div>
 );
 
